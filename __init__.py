1a29698d558726aae5ec337aec91d8338244a6f7
Mohamed <84293568+mohamedbtt9@users.noreply.github.com>
2024-06-03 10:21:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,11 @@ from flask import render_template
 from flask import json
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)    
+
+@app.route("/contact/")
+def MaPremiereAPI():
+    return "<h2>Ma page de contact</h2>"
                                                                                                                                        
 @app.route('/')
 def hello_world():
@@ -15,7 +19,7 @@ def exercices():
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+    return render_template("contact.html")
 
 @app.route('/calcul_carre/<int:val_user>')
 def carre(val_user):
