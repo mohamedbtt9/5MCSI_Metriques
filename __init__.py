3dfe7896899b559dae10986745bb54871c89a456
Mohamed <84293568+mohamedbtt9@users.noreply.github.com>
2024-06-03 11:08:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,12 +26,12 @@ def carre(val_user):
     return f"<h2>Le carré de votre valeur est : {res1}</h2><h3>Nombre impair</h3>"
 
 @app.route('/somme/<int:valeur1>/<int:valeur2>')
-def somme(val1,val2):
-  res = val1 + val2
-  if res % 2 == 0:
-    return f"<h2>Le resulat de la somme de {val1} + {valeur2} est : {resultat}</h2><h3>Nombre pair</h3>"
-  else:
-    return f"<h2>Le resulat de la somme de {val1} + {valeur2} est : {resultat}</h2><h3>Nombre impair</h3>"
+def somme(valeur1, valeur2):
+    res = valeur1 + valeur2
+    if res % 2 == 0:
+        return f"<h2>Le résultat de la somme de {valeur1} + {valeur2} est : {res}</h2><h3>Nombre pair</h3>"
+    else:
+        return f"<h2>Le résultat de la somme de {valeur1} + {valeur2} est : {res}</h2><h3>Nombre impair</h3>"
 
 
                                                                                                                
